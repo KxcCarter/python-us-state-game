bb88c20084241c46cad0834a760b4ffeb9648997
Kenneth Carter <kxccarter@gmail.com>
2021-04-27 13:22:57 -0500

    exports a csv of all states not guessed by user.

--- main.py
+++ main.py
@@ -19,16 +19,26 @@ while game_is_on:
 
     answer_state = screen.textinput(title=f"Guess a State - {num_states_guessed}/50", prompt="What's another state name?").title()
 
+    if answer_state == 'Exit':
+        break
+
     if answer_state in all_states:
         num_states_guessed += 1
         state_data = data[data.state == answer_state]
         title = Title()
         title.pin_title_to_map(int(state_data.x), int(state_data.y), answer_state)
+        all_states.remove(answer_state)
 
     if num_states_guessed == 50:
         game_is_on = False
         show_win = Title()
         show_win.annouce_win()
+
+
+new_state_data = pandas.DataFrame(all_states)
+new_state_data.to_csv("states_to_learn.csv")
+
+
 
 
 turtle.mainloop()
